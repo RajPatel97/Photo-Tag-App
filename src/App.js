75a29ee39e33e9f56b5e64d7b63313ee4872604f
RajPatel <raj191@live.com>
2021-08-11 15:41:02 -0400

    first version of the sidebar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import Header from "./componments/Header";
+import SideList from "./componments/SideList";
 function App() {
   return (
     <div className="App">
 
     <Header />
+    <SideList />
+
       
     </div>
   );
---
+++ src/componments/SideList.js
@@ -0,0 +1,11 @@
+const SideList = () => {
+    return ( 
+        <div className="sidenav">
+        <a href="">test1</a>
+        <a href="">test2</a>
+        <a href="">test3</a>
+        </div>
+     );
+}
+ 
+export default SideList;
