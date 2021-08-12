cb48529e2e5cd0ff9cfccc5625cc24ac328974a9
RajPatel <raj191@live.com>
2021-08-12 00:09:18 -0400

    adjusting the styles

--- src/componments/Image.js
+++ src/componments/Image.js
@@ -1,9 +1,15 @@
 import findimage from './assets/MortalKombat.jpeg'
 
 const Image = () => {
+
+    const handleClick=(event)=>{
+        console.log(event);
+        console.log(" X:"+event.pageX +" Y:"+event.pageY);
+    }
+
     return ( 
         <div className="image-wrapper">
-        <img src={findimage} alt="Find Them All!" />
+        <img src={findimage} alt="Find Them All!" onClick={handleClick}/>
         </div>
      );
 }
--- src/componments/SideList.js
+++ src/componments/SideList.js
@@ -1,6 +1,7 @@
 const SideList = () => {
     return ( 
         <div className="sidenav">
+        <h1>Find These Guys!</h1>
         <a href="test 3">test1</a>
         <a href="test 3">test2</a>
         <a href="test 3">test3</a>
