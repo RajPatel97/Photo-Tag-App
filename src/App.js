bceb273c4111faa97f7556bde40b8ec6a409017b
Raj Patel <raj191@live.com>
2021-08-12 22:03:37 -0400

    adding a custom cursor

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Header from "./componments/Header";
 import SideList from "./componments/SideList";
 import Image from "./componments/Image";
+import Cursor from "./componments/Cursor";
 function App() {
   return (
     <div className="App">
-
+    <Cursor />
     <Header />
     <div className="main-content">
       <SideList />
--- src/componments/Image.js
+++ src/componments/Image.js
@@ -1,5 +1,6 @@
 import findimage from './assets/MortalKombat.jpeg'
 
+
 const Image = () => {
 
     const handleClick=(event)=>{
