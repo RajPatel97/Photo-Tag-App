e82bfbd5207170addc1bb5798e2f97e25cc6f03e
RajPatel <raj191@live.com>
2021-08-16 23:36:39 -0400

    adding calls to the firebase database

--- src/componments/Image.js
+++ src/componments/Image.js
@@ -7,7 +7,8 @@ import "firebase/auth";
 import "firebase/firestore";
 import "firebase/database";
 
-firebase.initializeApp({
+//this is refered to later to get the data
+let app = firebase.initializeApp({
     apiKey: "AIzaSyCV73nCkr04lwKBMIh1IGc3zu7ZojVBK4U",
     authDomain: "photo-tag-69251.firebaseapp.com",
     projectId: "photo-tag-69251",
@@ -22,11 +23,29 @@ const Image = () => {
     const handleClick=(event)=>{
         //console.log(event);
         let database = firebase.database();
+        let db = firebase.firestore(app);
+        let characters= db.collection('Character Loactions').doc('dMSqvUfM4hW45HAtor2M');
+
+
+
+        //getting the data from the firebase collection 
+        //turn this into an async function later
+        characters.get().then((doc) => {
+            if (doc.exists) {
+                console.log("Document data:", doc.data());
+            } else {
+                // doc.data() will be undefined in this case
+                console.log("No such document!");
+            }
+        }).catch((error) => {
+            console.log("Error getting document:", error);
+        });
+
+        //printing the x and y of the page on click of the cursor 
         console.log(" X:"+event.pageX +" Y:"+event.pageY);
-        console.log(database);
+
         //console.log(" X:"+event.clientX +" Y:"+event.clientY);
     }
-
     return ( 
         <div className="image-wrapper">
         <img src={findimage} alt="Find Them All!" onClick={handleClick}/>
