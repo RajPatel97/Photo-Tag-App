ca12e1a259290132a07851464f61966975181fc5
RajPatel <raj191@live.com>
2021-08-18 17:48:54 -0400

    making an update

--- src/componments/Image.js
+++ src/componments/Image.js
@@ -7,7 +7,7 @@ import "firebase/auth";
 import "firebase/firestore";
 import "firebase/database";
 
-//this is refered to later to get the data
+//this is refereed to later to get the data
 let app = firebase.initializeApp({
     apiKey: "AIzaSyCV73nCkr04lwKBMIh1IGc3zu7ZojVBK4U",
     authDomain: "photo-tag-69251.firebaseapp.com",
@@ -17,21 +17,23 @@ let app = firebase.initializeApp({
     appId: "1:133774276293:web:2da2dc09aef9c9cfc162ac"
   });
 
+
+
 const Image = () => {
-    
 
-    const handleClick=(event)=>{
-        //console.log(event);
-        let db = firebase.firestore(app);
+
+    const getData =  function(){
+    let db = firebase.firestore(app);
         let characters= db.collection('Character Loactions').doc('dMSqvUfM4hW45HAtor2M');
-
-
 
         //getting the data from the firebase collection 
         //turn this into an async function later
         characters.get().then((doc) => {
             if (doc.exists) {
-                console.log("Document data:", doc.data());
+                let data = doc.data();
+                console.log(data['Erron Black']);//making a ref to the data
+                
+                
             } else {
                 // doc.data() will be undefined in this case
                 console.log("No such document!");
@@ -39,14 +41,17 @@ const Image = () => {
         }).catch((error) => {
             console.log("Error getting document:", error);
         });
+  }
 
+
+
+    const handleClick=(event)=>{
         //printing the x and y of the page on click of the cursor 
         console.log(" X:"+event.pageX +" Y:"+event.pageY);
-
-        //console.log(" X:"+event.clientX +" Y:"+event.clientY);
     }
     return ( 
         <div className="image-wrapper">
+        {getData()}
         <img src={findimage} alt="Find Them All!" onClick={handleClick}/>
         </div>
      );
